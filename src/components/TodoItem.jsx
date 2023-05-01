c6cfc9e62f35646d758b2c98cbab31190369dd4a
Naji-97 <elbairabennaji@gmail.com>
2023-05-01 18:58:07 +0100

    add style

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import styles from '../styles/TodoItem.module.css';
+import styles from '../styles/TodoItem.module.css';
 
 function TodoItem({
   itemProp, handleChange, deleTodo,
